bd223612d2a0a6b2da15f56d8b1f371109065da3
LGM1213 <lgm1213@gmail.com>
2016-11-26 16:56:24 -0500

    added yelp api index page styled a bit

---
+++ config/initializers/yelp.rb
@@ -0,0 +1,6 @@
+Yelp.client.configure do |config|
+  config.consumer_key = "Ds8JtFCghy-1HXBImyMIrQ"
+  config.consumer_secret =  "wh8ziD1dF1wJq_CUcMYTuF_PYq8"
+  config.token = "lxpnBH3DVzym63rES1hiRMsPT0VQ-eX7"
+  config.token_secret = "0tO9PX9ufrzZ03VzaEFjefJAvGU"
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
-  resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'yelp/index'
+  root 'yelp#index'
+ #  get 'home/index'
+
+ #  resources :users
+	# root to: 'yelp#index'
+
+ #    # These routes will be for signup. The first renders a form in the browse, the second will 
+ #    # receive the form and create a user in our database using the data given to us by the user.
+ #    get '/signup' => 'users#new'
+ #    post '/users' => 'users#create'
+
 end
